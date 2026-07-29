0f9c8caa50dd6ee7653f218de24798819e71bb2f
evibcyber <evilija.buciute@gmail.com>
2026-07-29 17:07:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "journal-v2";
+const CACHE = "journal-v3";
 const FILES = ["./", "./index.html", "./manifest.webmanifest",
                "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png"];
 
